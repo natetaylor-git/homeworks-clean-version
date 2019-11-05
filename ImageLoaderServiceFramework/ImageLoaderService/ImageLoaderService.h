93377e877f93e25484a28c7d86d37bcf435e8390
Vsevolod <natetaylor.ggl@gmail.com>
2019-11-05 16:46:20 +0300

    [hw8] Создан пустой фреймворк для Carthage

---
+++ ImageLoaderServiceFramework/ImageLoaderService/ImageLoaderService.h
@@ -0,0 +1,19 @@
+//
+//  ImageLoaderService.h
+//  ImageLoaderService
+//
+//  Created by nate.taylor_macbook on 05/11/2019.
+//  Copyright © 2019 natetaylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageLoaderService.
+FOUNDATION_EXPORT double ImageLoaderServiceVersionNumber;
+
+//! Project version string for ImageLoaderService.
+FOUNDATION_EXPORT const unsigned char ImageLoaderServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageLoaderService/PublicHeader.h>
+
+
